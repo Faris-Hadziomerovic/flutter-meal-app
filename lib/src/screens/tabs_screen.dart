1657dd306a7e7e6495f60d29ababa0e365c0ef14
Faris Hadžiomerović <faris.hadziomerovic@edu.fit.ba>
2022-12-07 01:26:18 +0100

    added bottom navigation

--- lib/src/screens/tabs_screen.dart
+++ lib/src/screens/tabs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 
 import './categories_screen.dart';
 import './favourites_screen.dart';
@@ -11,48 +12,99 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': const CategoriesScreen(),
+      'title': 'Categories',
+      'icon': const Icon(Icons.category_outlined),
+      'activeIcon': const Icon(Icons.category),
+    },
+    {
+      'page': const FavouritesScreen(),
+      'title': 'Favourites',
+      'icon': const Icon(Icons.favorite_border),
+      'activeIcon': const Icon(Icons.favorite),
+    },
+  ];
+
+  int _pageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _pageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Deli meals'),
-          bottom: TabBar(
-            splashBorderRadius: const BorderRadius.vertical(
-              top: Radius.circular(20),
-            ),
-            tabs: [
-              Tab(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.category_outlined),
-                    SizedBox(width: 10),
-                    Text('Categories'),
-                  ],
-                ),
-              ),
-              Tab(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.favorite_outline),
-                    SizedBox(width: 15),
-                    Text('Favourites'),
-                  ],
-                ),
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _pages[_pageIndex]['title'] as String,
         ),
-        body: const TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavouritesScreen(),
-          ],
-        ),
+      ),
+      body: _pages[_pageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        backgroundColor: Theme.of(context).primaryColor,
+        onTap: _selectPage,
+        currentIndex: _pageIndex,
+        items: [
+          ..._pages.mapIndexed((i, page) {
+            return BottomNavigationBarItem(
+              label: _pages[i]['title'] as String,
+              icon: _pages[i]['icon'] as Icon,
+              activeIcon: _pages[i]['activeIcon'] as Icon,
+            );
+          }),
+        ],
       ),
     );
   }
+}
+
+Widget buildTopSideTabbedPage() {
+  return DefaultTabController(
+    length: 2,
+    child: Scaffold(
+      appBar: AppBar(
+        title: const Text('Deli meals'),
+        bottom: TabBar(
+          splashBorderRadius: const BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+          tabs: [
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.category_outlined),
+                  SizedBox(width: 10),
+                  Text('Categories'),
+                ],
+              ),
+            ),
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.favorite_outline),
+                  SizedBox(width: 15),
+                  Text('Favourites'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: const TabBarView(
+        children: [
+          CategoriesScreen(),
+          FavouritesScreen(),
+        ],
+      ),
+    ),
+  );
 }
